4c02f1784e927ec2641855c10259b3c8c1cfdcd1
Chris Angelico <rosuav@gmail.com>
2015-02-24 00:32:49 +1100

    RecvDir: Add installation mode

--- recvdir.py
+++ recvdir.py
@@ -1,4 +1,27 @@
 # Counterpart to senddir.py
+import sys
+if len(sys.argv)>1 and sys.argv[1]=="install":
+	import os
+	open("/etc/systemd/system/recvdir.service","w").write("""[Unit]
+Description=Teleportation Arrivals
+
+[Service]
+# The user and path are derived at installation time
+# from SUDO_USER and current working directory.
+User=%s
+WorkingDirectory=%s
+ExecStart=%s %s
+# If the network isn't available yet, restart until it is.
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=multi-user.target
+"""%(os.environ["SUDO_USER"],os.getcwd(),sys.executable,os.path.abspath(sys.argv[0])))
+	print("Installed as recvdir.service.")
+	exit()
+
+# Main program continues below.
 import socketserver
 
 class Handler(socketserver.StreamRequestHandler):
