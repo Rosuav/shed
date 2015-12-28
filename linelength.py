7241c87472714f1d046cd1f122549a7062f7bf86
Chris Angelico <rosuav@gmail.com>
2015-12-28 14:16:57 +1100

    linelength: New script to help track down those overly-long lines of code

---
+++ linelength.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import argparse
+args = argparse.ArgumentParser(description="Line length checker")
+args.add_argument("-l", "--length", default=80, type=int, help="Warn if a line exceeds this maximum")
+args.add_argument("-1", "--once", action="store_true", help="Show only the one worst line for each file")
+args.add_argument("files", nargs="*")
+args = args.parse_args()
+for fn in args.files:
+	max = args.length
+	msg = ""
+	with open(fn, encoding="utf-8") as f:
+		for i, line in enumerate(f, 1):
+			line = line.rstrip("\n").expandtabs()
+			if len(line) > max:
+				msg = "%s:%d:%s" % (fn, i, line)
+				if args.once: max = len(line)
+				else: print(msg)
+	if args.once and msg: print(msg)
