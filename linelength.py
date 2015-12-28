54cf7d688053046d1cfc6bf5f1ffbe4b013f98d1
Chris Angelico <rosuav@gmail.com>
2015-12-28 14:19:10 +1100

    linelength: Allow custom tab width

--- linelength.py
+++ linelength.py
@@ -2,6 +2,7 @@
 import argparse
 args = argparse.ArgumentParser(description="Line length checker")
 args.add_argument("-l", "--length", default=80, type=int, help="Warn if a line exceeds this maximum")
+args.add_argument("-t", "--tabs", default=8, type=int, help="Interpret tabs as this many characters")
 args.add_argument("-1", "--once", action="store_true", help="Show only the one worst line for each file")
 args.add_argument("files", nargs="*")
 args = args.parse_args()
@@ -10,7 +11,7 @@ for fn in args.files:
 	msg = ""
 	with open(fn, encoding="utf-8") as f:
 		for i, line in enumerate(f, 1):
-			line = line.rstrip("\n").expandtabs()
+			line = line.rstrip("\n").expandtabs(args.tabs)
 			if len(line) > max:
 				msg = "%s:%d:%s" % (fn, i, line)
 				if args.once: max = len(line)
