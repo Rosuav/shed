32b6635d529c9e4a975c616239d1e5a7427bc120
Chris Angelico <rosuav@gmail.com>
2016-12-19 15:08:51 +1100

    chalices: En-repo some mathematics about Murder Trivia

---
+++ chalices.py
@@ -0,0 +1,35 @@
+# Calculate your odds of surviving the Murder Trivia Party chalices game
+from collections import defaultdict
+
+CHALICES = 8 # Total number of chalices
+chances = {0: 1.0} # Initially, you have 100% chance of having no placed pellets.
+total_chances = []
+for poison in range(CHALICES): # though you won't normally see the full set
+	# Add another poison pellet.
+	newchance = defaultdict(float)
+	for poisoned, prob in chances.items():
+		# There are 'poisoned' chalices already poisoned when this pellet
+		# gets added. Thus there are that many chances out of CHALICES to
+		# re-poison a poisoned chalice.
+		if poisoned:
+			newchance[poisoned] += prob * poisoned / CHALICES
+		# The remaining probability is that you poisoned a new one.
+		true_brew = CHALICES - poisoned
+		if true_brew:
+			newchance[poisoned + 1] += prob * true_brew / CHALICES
+		# This will always add up to the original probability.
+		# The total probability in the dictionary will always equal 1.0.
+	chances = newchance
+	print("With %d poison pellets, your chances are:" % (poison + 1))
+	tot = 0.0
+	for poisoned, prob in sorted(chances.items()):
+		survive = 100 - poisoned * 100.0 / CHALICES
+		print("%5.2f%% chance of %d poisoned for %5.2f%% survival chance ==> %5.2f%%"
+			% (prob*100, poisoned, survive, survive * prob))
+		tot += survive * prob
+	print("-- total probability of survival %5.2f" % tot)
+	total_chances.append(tot)
+
+print("Summary:")
+print(" ", "  ".join(" %2s   " % (n+1) for n in range(CHALICES)))
+print(" ", "  ".join("%5.2f%%" % c for c in total_chances))
