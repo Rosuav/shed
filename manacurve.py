d471cd948b07d7ae9e16cb7934abc0590ffaacdb
Chris Angelico <rosuav@gmail.com>
2017-05-19 06:39:18 +1000

    manacurve: Start a new toy project

---
+++ manacurve.py
@@ -0,0 +1,14 @@
+# Mana curve prediction/stats
+"""
+Gavin Verhey says: "More than any other way, I have seen a larger number of
+Limited games won by simply curving out with drops on turns two through five
+than any other method. If you hit your drops each turn and your opponent
+stumbles, they're quickly going to be put on the back foot."
+
+http://magic.wizards.com/en/articles/archive/beyond-basics/how-build-mana-curve-2017-05-18
+
+Okay. So let's suppose you JUST focus on that. How would you build a deck to
+have the best possible chances of hitting creatures turns two through five?
+
+Let's find out.
+"""
