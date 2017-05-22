4c65023cf7826f615cf85223cc75e1d16345b960
Chris Angelico <rosuav@gmail.com>
2017-05-22 22:36:43 +1000

    uniname: Add support for multi-word expressions

--- uniname.py
+++ uniname.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 import sys, unicodedata
-for ch in sys.argv[1]: print(ascii(ch), unicodedata.name(ch))
+for ch in " ".join(sys.argv[1:]): print(ascii(ch), unicodedata.name(ch))
