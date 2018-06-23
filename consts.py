58a6b5bd3344f274f70a27892836dfe4b14f3521
Chris Angelico <rosuav@gmail.com>
2018-06-23 15:12:47 +1000

    consts: Attempted implementation, first cut. Seems to kinda work.

--- consts.py
+++ consts.py
@@ -1,4 +1,5 @@
 import dis
+import opcode
 
 class InvalidConstantError(Exception): pass
 
@@ -10,7 +11,47 @@ def const(**names):
 		print(x) # prints 1
 	"""
 	def decorate(func):
-		...
+		# Constants are not allowed to be assigned to within the function.
+		# Thus they should never appear in the function's local names.
+		# TODO: Alter the message if the colliding name is an argument.
+		c = func.__code__
+		if set(names) & set(c.co_varnames):
+			raise InvalidConstantError("Cannot assign to local constant")
+
+		# Constants should not be declared nonlocal and then assigned to.
+		# (Unfortunately this doesn't actually check if they were assigned to.)
+		if set(names) & set(c.co_freevars):
+			raise InvalidConstantError("Cannot create local and nonlocal constant")
+
+		# Constants should therefore be global names.
+		if set(names) - set(c.co_names):
+			raise InvalidConstantError("Constant not referenced as global")
+
+		# Okay. So now we replace every LOAD_GLOBAL for one of these names
+		# with a LOAD_CONST.
+		newcode = []
+		newconsts = c.co_consts
+		for name, val in names.items():
+			try:
+				names[name] = newconsts.index(val)
+			except ValueError:
+				names[name] = len(newconsts)
+				newconsts += (val,)
+		LOAD_CONST = opcode.opmap["LOAD_CONST"]
+		for instr in dis.get_instructions(c):
+			if instr.opname == "LOAD_GLOBAL" and instr.argval in names:
+				newcode.append(LOAD_CONST)
+				newcode.append(names[instr.argval])
+			else:
+				newcode.append(instr.opcode)
+				newcode.append(instr.arg or 0)
+
+		codeobj = type(c)(c.co_argcount, c.co_kwonlyargcount, c.co_nlocals, c.co_stacksize,
+			c.co_flags, bytes(newcode), newconsts, c.co_names, c.co_varnames, c.co_filename,
+			c.co_name, c.co_firstlineno, c.co_lnotab, c.co_freevars, c.co_cellvars)
+
+		func = type(func)(codeobj, func.__globals__, func.__name__, func.__defaults__, func.__closure__)
+
 		return func
 	return decorate
 
