bf896c3d02be682c34bcd7cc885901da29fbf020
Chris Angelico <rosuav@gmail.com>
2019-03-12 00:29:53 +1100

    mines: Begin implementing an autosolver

--- mines.js
+++ mines.js
@@ -24,7 +24,7 @@ function build(tag, attributes, children) {
 	return ret;
 }
 
-function dig(r, c) {
+function dig(game, r, c) {
 	const num = game[r][c];
 	if (num > 9) return; //Already dug
 	const btn = board.children[r].children[c].firstChild;
@@ -41,7 +41,7 @@ function dig(r, c) {
 		btn.classList.add("flat"); //Don't show the actual zero
 		for (let dr = -1; dr <= 1; ++dr) for (let dc = -1; dc <= 1; ++dc) {
 			if (r+dr < 0 || r+dr >= height || c+dc < 0 || c+dc >= width) continue;
-			dig(r+dr, c+dc);
+			dig(game, r+dr, c+dc);
 		}
 		return;
 	}
@@ -50,7 +50,7 @@ function dig(r, c) {
 
 function clicked(ev) {
 	const btn = ev.currentTarget;
-	dig(+btn.dataset.r, +btn.dataset.c);
+	dig(game, +btn.dataset.r, +btn.dataset.c);
 	btn.blur();
 }
 
@@ -82,6 +82,44 @@ function generate_game() {
 	return game;
 }
 
+function get_unknowns(game, r, c) {
+	//Helper for try_solve - get an array of the unknown cells around a cell
+	//Returns [n, [r,c], [r,c], [r,c]] with any number of row/col pairs
+	if (game[r][c] < 10) return null; //Shouldn't happen
+	const ret = [game[r][c]];
+	for (let dr = -1; dr <= 1; ++dr) for (let dc = -1; dc <= 1; ++dc) {
+		if (r+dr < 0 || r+dr >= height || c+dc < 0 || c+dc >= width) continue;
+		const cell = game[r+dr][c+dc];
+		if (cell < 10) ret.push([r+dr, c+dc]);
+		if (cell === 19) ret[0]--;
+	}
+	return ret;
+}
+
+//Try to solve the game. Duh :)
+//Algorithm is pretty simple. Build an array of regions
+function try_solve(game) {
+	//First, build up a list of trivial regions.
+	for (let r = 0; r < height; ++r) for (let c = 0; c < width; ++c) {
+		if (game[r][c] < 10) continue;
+		const region = get_unknowns(game, r, c);
+		if (region.length === 1) continue; //No unknowns
+		if (region[0] === 0) {
+			//There are no unflagged mines in this region!
+			for (let i = 1; i < region.length; ++i)
+				dig(game, region[i][0], region[i][1]);
+		}
+		if (region[1] === region.length - 1)
+		{
+			//There are as many unflagged mines as unknowns!
+			//Unimplemented
+			//for (let i = 1; i < region.length; ++i)
+				//flag(game, region[i][0], region[i][1]);
+		}
+	}
+	return true;
+}
+
 function new_game() {
 	const table = [];
 	for (let r = 0; r < height; ++r) {
@@ -91,8 +129,13 @@ function new_game() {
 		table.push(build("tr", 0, tr));
 	}
 	set_content(board, table);
-	game = generate_game();
-	dig(0, 0);
+	while (true) {
+		const tryme = generate_game();
+		if (!try_solve(tryme)) continue;
+		game = tryme;
+		break;
+	}
+	dig(game, 0, 0);
 	console.log(game);
 }
 
