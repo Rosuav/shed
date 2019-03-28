3a7459f0f9e171954eb1f86a9e320ef889b9d1a5
Chris Angelico <rosuav@gmail.com>
2019-03-29 03:31:55 +1100

    watch_emotes: Tidy up and en-repo my little monitor

---
+++ watch_emotes.py
@@ -0,0 +1,24 @@
+import sys
+import time
+import pprint
+import subprocess
+import requests
+sys.path.append("../mustard-mine")
+import config
+
+emote_count = None
+while emote_count is None or emote_count == len(emotes):
+	req = requests.get("https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=317117,317121,317122", headers={
+		"Client-ID": config.CLIENT_ID,
+		"Accept": "application/vnd.twitchtv.v5+json",
+	})
+	resp = req.json()
+	emotes = {e["code"]: e["id"] for s in resp["emoticon_sets"].values() for e in s}
+	resp = requests.get("https://api.betterttv.net/2/channels/rosuav").json()
+	emotes.update({e["code"]: e["id"] for e in resp["emotes"]})
+	pprint.pprint(emotes)
+	# Whatever number there are on the first run, that's considered "current".
+	# If it then changes, report it.
+	if emote_count is None: emote_count = len(emotes)
+	time.sleep(60)
+subprocess.check_call(["vlc", "/video/Clips/Let It Go/Turkish - Aldirma.mkv"])
