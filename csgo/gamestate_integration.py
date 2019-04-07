127a1c3f5ecdaff2253912023ecba45514a39473
Chris Angelico <rosuav@gmail.com>
2019-04-07 14:51:59 +1000

    Show a bit more debug info

--- csgo/gamestate_integration.py
+++ csgo/gamestate_integration.py
@@ -38,6 +38,7 @@ configs = {
 	},
 }
 
+last_known_cfg = {} # Mainly for the sake of logging
 @app.route("/", methods=["POST"])
 def update_configs():
 	if not request.json: return "", 400
@@ -46,11 +47,14 @@ def update_configs():
 		data = request.json
 		# If any part of the path isn't found, data will be None
 		for key in path: data = data and data.get(key)
+		if data == last_known_cfg.get(path): continue
+		last_known_cfg[path] = data
+		logging.log(24, "New value for %s: %s", "-".join(path), data)
 		cfg = options.get(data, options.get(..., ""))
 		filename = "gsi_" + "_".join(path) + ".cfg"
-		# For simplicity, we read from the filesystem every time.
-		# This may cause performance problems (eg on Windows); if
-		# so, consider adding a local cache.
+		# We read from the filesystem every time. The last_known_cfg
+		# cache will show changes that don't affect the actual state,
+		# but those should not trigger the other handlers.
 		try:
 			with open(filename) as f: prevcfg = f.read()
 		except FileNotFoundError:
@@ -65,6 +69,6 @@ def update_configs():
 
 if __name__ == "__main__":
 	import logging
-	logging.basicConfig(level=25) # use logging.INFO to see timestamped lines every request
+	logging.basicConfig(level=24) # use logging.INFO to see timestamped lines every request
 	import os; logging.log(25, "I am %d / %d", os.getuid(), os.getgid())
 	app.run(host="127.0.0.1", port=27014)
