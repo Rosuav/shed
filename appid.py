cc2890ec89dbce019fb623be72a453a01aaef00b
Chris Angelico <rosuav@gmail.com>
2019-06-13 20:22:21 +1000

    appid: Cache the mapping of name to appid

--- appid.py
+++ appid.py
@@ -8,15 +8,16 @@ CACHE_FILE = os.path.abspath(__file__ + "/../appid.json")
 
 try:
 	with open(CACHE_FILE) as f:
-		data = json.load(f)
+		appids = json.load(f)
 except FileNotFoundError:
 	import requests # ImportError? pip install requests
 	print("Downloading Steam appid list...")
 	r = requests.get("https://api.steampowered.com/ISteamApps/GetAppList/v0001/")
 	r.raise_for_status()
 	data = r.json()
+	appids = {app["name"]: app["appid"] for app in data["applist"]["apps"]["app"]}
 	with open(CACHE_FILE, "w") as f:
-		json.dump(data, f)
+		json.dump(appids, f)
 	print("Downloaded and cached.")
 
 if len(sys.argv) == 1:
