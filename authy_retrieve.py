8ffa27e2d43e78ec9ad3ef97f6e7323727a58d2f
Chris Angelico <rosuav@gmail.com>
2019-07-12 21:22:21 +1000

    authy-retrieve: Scratch that. Failed project idea.

--- authy_retrieve.py
+++ authy_retrieve.py
@@ -8,3 +8,12 @@
 # This app works by creating a new device for an existing user, and then
 # asking Authy servers to synchronize with it. Will require the user's
 # master password or perhaps backup password.
+
+# May need to also synchronize Google Authenticator apps?
+# /json/users/USERID/authenticator_tokens
+
+# /json/users/USERID/devices/DEVICEID/apps/sync
+
+# Would need to create a device. Would need to have an API key.
+# And Twilio's APIs are no fun to play with. I think I'd rather go spork
+# my eyes out than figure out what they're trying to get me to do.
