f0d5dae0823f083dfe66caae12960ee32fc32967
Chris Angelico <rosuav@gmail.com>
2019-07-14 12:02:36 +1000

    ahm: Create a file with nothing but a TODO in it

---
+++ autohost_manager.py
@@ -0,0 +1,9 @@
+"""
+TODO Mon?: Autohost manager for AliCatFiberarts (and others).
+* Have a list of high priority streams, in order (or with priorities)
+* Listen for, or poll for, streams going live
+* If (a) a high priority stream has just gone live, and (b) you are currently hosting, and (c) the hosted stream has lower priority
+* Then send "/unhost" to the channel.
+* Have a very very simple GUI (tkinter?)
+* "Optional: Rename this to autohost_manager.pyw to hide the black box"
+"""
