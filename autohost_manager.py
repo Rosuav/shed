4cd4a6aee6eef9cbb9bb3934bba836dcfa485468
Chris Angelico <rosuav@gmail.com>
2019-07-15 23:09:59 +1000

    ahm: Lay out a battle plan

--- autohost_manager.py
+++ autohost_manager.py
@@ -7,3 +7,13 @@ TODO Mon?: Autohost manager for AliCatFiberarts (and others).
 * Have a very very simple GUI (tkinter?)
 * "Optional: Rename this to autohost_manager.pyw to hide the black box"
 """
+
+# Components needed:
+# 1) Hosting control via IRC
+# 2) Going-live detection
+# 2a) Poll at a set interval eg 15 mins
+# 2b) Receive webhook notifications from Twitch
+# 3) Authentication, mainly for IRC
+# 3b) Optionally allow user to override channel name (in case you're an editor)
+# 4) Configuration of channel priorities, since we can't query Twitch
+# 5) JSON config storage
