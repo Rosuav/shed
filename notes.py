9c8bb5853bfaa4b8c9d052cb0392558ff650d96a
Chris Angelico <rosuav@gmail.com>
2019-09-13 20:53:39 +1000

    notes: Flesh out into something mostly useful

--- notes.py
+++ notes.py
@@ -1,6 +1,23 @@
 #!/usr/bin/python3
 # NOTE: May require the system Python 3 rather than using 3.9
+import os.path
+import sys
 import speech_recognition as sr
+
+NOTES_DIR = os.path.expanduser(os.environ.get("NOTES_DIR", "~/tmp/notes"))
+os.makedirs(NOTES_DIR, exist_ok=True)
+blocks = sorted(fn for fn in os.listdir(NOTES_DIR) if fn != "notes.log")
+
+if not blocks or "--new-block" in sys.argv:
+	next = int(blocks[-1] if blocks else 0) + 1
+	blocks.append(str(next))
+	os.mkdir(NOTES_DIR + "/" + blocks[-1])
+block = NOTES_DIR + "/" + blocks[-1] # Use the latest block (which we may have just created)
+
+notes = os.listdir(block)
+notes.sort()
+note_id = int(notes[-1].split("-")[0]) + 1 if notes else 1
+
 r = sr.Recognizer()
 # Can I increase the gain at all?
 with sr.Microphone() as source:
@@ -12,8 +29,9 @@ with sr.Microphone() as source:
 	# )
 print("Got notes.")
 
-import random
-with open("notes%d.flac" % random.randrange(10000), "wb") as f: f.write(audio.get_flac_data())
+desc = " ".join(sys.argv[1:])
+fn = "%02d - " % note_id + desc
+with open(block + "/%s.flac" % fn, "wb") as f: f.write(audio.get_flac_data())
 
 try: sphinx = r.recognize_sphinx(audio)
 except sr.UnknownValueError: sphinx = ""
@@ -24,5 +42,7 @@ try: google = r.recognize_google(audio)
 except sr.UnknownValueError: google = ""
 except sr.RequestError as e: google = repr(e)
 
-print("Sphinx:", sphinx)
-print("Google:", google)
+with open(NOTES_DIR + "/notes.log", "a") as f:
+	print("[%s]" % block, fn, file=f)
+	print("Sphinx:", sphinx, file=f)
+	print("Google:", google, file=f)
