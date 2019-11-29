ad4a5364f722b6b1073650f11e967627a0e16efe
Chris Angelico <rosuav@gmail.com>
2019-11-30 10:10:59 +1100

    watch_emotes: Also watch Stephen's emotes, and give more info

--- watch_emotes.py
+++ watch_emotes.py
@@ -8,18 +8,21 @@ import config
 
 emote_count = None
 while emote_count is None or emote_count == len(emotes):
-	req = requests.get("https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=317117,317121,317122", headers={
+	req = requests.get("https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=317117,317121,317122,1184925,1184926,1184927", headers={
 		"Client-ID": config.CLIENT_ID,
 		"Accept": "application/vnd.twitchtv.v5+json",
 	})
 	resp = req.json()
 	emotes = {e["code"]: e["id"] for s in resp["emoticon_sets"].values() for e in s}
-	resp = requests.get("https://api.betterttv.net/2/channels/rosuav").json()
-	emotes.update({e["code"]: e["id"] for e in resp["emotes"]})
+	# Optionally also watch for BTTV emotes
+	#resp = requests.get("https://api.betterttv.net/2/channels/rosuav").json()
+	#emotes.update({e["code"]: e["id"] for e in resp["emotes"]})
 	pprint.pprint(emotes)
 	# Whatever number there are on the first run, that's considered "current".
 	# If it then changes, report it.
 	if emote_count is None: emote_count = len(emotes)
 	elif emote_count != len(emotes): break # Don't wait for the next minute
 	time.sleep(60)
+print("====================")
+pprint.pprint(emotes)
 subprocess.check_call(["vlc", "/video/Clips/Let It Go/Turkish - Aldirma.mkv"])
