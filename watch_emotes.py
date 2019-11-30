b596bf514a6eaaf3376753fa485600ac92458c39
Chris Angelico <rosuav@gmail.com>
2019-11-30 13:11:07 +1100

    watch_emotes: Remove unnecessary print at the end, and suggest a way to use this cleanly

--- watch_emotes.py
+++ watch_emotes.py
@@ -17,12 +17,10 @@ while emote_count is None or emote_count == len(emotes):
 	# Optionally also watch for BTTV emotes
 	#resp = requests.get("https://api.betterttv.net/2/channels/rosuav").json()
 	#emotes.update({e["code"]: e["id"] for e in resp["emotes"]})
-	pprint.pprint(emotes)
+	pprint.pprint(emotes) # Tip: Size the terminal window to nicely fit this.
 	# Whatever number there are on the first run, that's considered "current".
 	# If it then changes, report it.
 	if emote_count is None: emote_count = len(emotes)
 	elif emote_count != len(emotes): break # Don't wait for the next minute
 	time.sleep(60)
-print("====================")
-pprint.pprint(emotes)
 subprocess.check_call(["vlc", "/video/Clips/Let It Go/Turkish - Aldirma.mkv"])
