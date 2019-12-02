9735dcc16214921dd5eff6238057137af0b5ec88
Chris Angelico <rosuav@gmail.com>
2019-12-02 15:06:52 +1100

    auto-volume: Populate the cache

--- auto-volume.py
+++ auto-volume.py
@@ -1,6 +1,7 @@
 # Build auto-volume.lua from the template and a JSON file of data
 import os
 import re
+import json
 import argparse
 import pydub
 
@@ -8,8 +9,8 @@ import pydub
 # directories will be recursed into. Unreadable or unparseable files
 # will be ignored.
 
+CACHE_FILE = os.path.abspath(os.path.join(__file__, "../auto-volume.json"))
 file_info = {}
-# TODO: Read from auto-volume.json
 
 def parse_file(fn, *, force=False):
 	fn = os.path.abspath(fn)
@@ -17,6 +18,8 @@ def parse_file(fn, *, force=False):
 	try:
 		audio = pydub.AudioSegment.from_file(fn)
 		print("%s: %.2f dB (max %.2f)" % (fn, audio.dBFS, audio.max_dBFS))
+		file_info[fn] = audio.dBFS
+		file_info[...] = True
 		# TODO: Also figure out if there's leading silence
 	except pydub.exceptions.CouldntDecodeError:
 		print(fn, "... unable to parse")
@@ -34,8 +37,15 @@ if __name__ == "__main__":
 	parser.add_argument("--play", help="Invoke VLC after parsing", action="store_true")
 	parser.add_argument("paths", metavar="file", nargs='+', help="File/dir to process")
 	args = parser.parse_args()
+	try:
+		with open(CACHE_FILE) as f: file_info = json.load(f)
+	except FileNotFoundError: pass
 	for path in args.paths:
 		if os.path.isdir(path): parse_dir(path, force=args.all)
 		else: parse_file(path, force=args.all)
+	if ... in file_info: # Dirty flag
+		del file_info[...] # Remove the flag from the cache file
+		with open(CACHE_FILE, "w") as f:
+			json.dump(file_info, f)
 	if args.play:
 		os.execvp("vlc", args.paths)
