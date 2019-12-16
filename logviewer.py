9f679eccc61ac2b5957f54c751000c349a8f8e88
Chris Angelico <rosuav@gmail.com>
2019-12-17 00:29:29 +1100

    logviewer: Replace stub with a simple exec-to-sudo

--- logviewer.py
+++ logviewer.py
@@ -7,7 +7,7 @@ import os
 while True:
 	print("1. View DHCP logs")
 	print("2. Tail DHCP logs")
-	print("3. Enter TOTP")
+	print("3. Root shell (requires authentication)")
 	print("0. Exit")
 	cmd = input("Pick an action: ")
 	if cmd == "1":
@@ -16,7 +16,8 @@ while True:
 		os.execlp("journalctl", "journalctl", "-fuisc-dhcp-server")
 		print("** Unable to exec to journalctl **")
 	elif cmd == "3":
-		print("Not yet implemented")
+		os.execlp("sudo", "sudo", "-i")
+		print("** Unable to exec to sudo **")
 	elif cmd == "0":
 		break
 	else:
