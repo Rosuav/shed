ebee0ad6a30f06fe8de90a8dac58234a8ba18841
Chris Angelico <rosuav@gmail.com>
2019-12-17 00:53:08 +1100

    logviewer: Allow (non-root) shell access if permissions granted

--- logviewer.py
+++ logviewer.py
@@ -7,7 +7,7 @@ import os
 while True:
 	print("1. View DHCP logs")
 	print("2. Tail DHCP logs")
-	print("3. Root shell (requires authentication)")
+	print("149. Shell (requires authentication)")
 	print("0. Exit")
 	cmd = input("Pick an action: ")
 	if cmd == "1":
@@ -16,8 +16,14 @@ while True:
 		os.execlp("journalctl", "journalctl", "-fuisc-dhcp-server")
 		print("** Unable to exec to journalctl **")
 	elif cmd == "3":
-		os.execlp("sudo", "sudo", "-i")
-		print("** Unable to exec to sudo **")
+		print("Provide credentials to get shell access:")
+		r = subprocess.run(["sudo", "-v"])
+		if r.returncode: continue # Assume sudo already printed an error
+		# Note that this will cause sudo to cache the credentials,
+		# granting passwordless sudo for the next fifteen minutes (or
+		# whatever the sysadmin has configured).
+		os.execlp("/bin/bash", "bash")
+		print("** Unable to exec to bash **")
 	elif cmd == "0":
 		break
 	else:
