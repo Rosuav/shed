9e92c9fd9814f05e36b5dddafac0fed5fcd92e73
Chris Angelico <rosuav@gmail.com>
2020-01-24 21:23:52 +1100

    updates: Stuff all the code into main(), can always refactor later

--- updates.py
+++ updates.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
 # requires system Python and the python3-apt package
-import sys
 from collections import OrderedDict # Starting with Python 3.7, we could just use vanilla dicts
 import apt # ImportError? apt install python3-apt
 
-cache = apt.Cache()
-cache.open()
-upgrades = []
-auto = 0
-for pkg in cache:
-	if not pkg.is_installed: continue # This is checking upgrades only
-	if pkg.candidate == pkg.installed: continue # Already up-to-date
-	if pkg.is_auto_installed:
-		# Ignore (but summarize) autoinstalled packages
-		auto += 1
-		continue
-	upgrades.append(pkg)
-if not upgrades:
-	print("Everything up-to-date.")
-	sys.exit(0)
+def main():
+	cache = apt.Cache()
+	cache.open()
+	upgrades = []
+	auto = 0
+	for pkg in cache:
+		if not pkg.is_installed: continue # This is checking upgrades only
+		if pkg.candidate == pkg.installed: continue # Already up-to-date
+		if pkg.is_auto_installed:
+			# Ignore (but summarize) autoinstalled packages
+			auto += 1
+			continue
+		upgrades.append(pkg)
+	if not upgrades:
+		print("Everything up-to-date.")
+		return
 
-def describe(pkg):
-	# Python 3.7 equivalent:
-	# return {"Name": pkg.name, "Installed": pkg.installed.version, "Candidate": pkg.candidate.version}
-	return OrderedDict((("Name", pkg.name), ("Current", pkg.installed.version), ("Target", pkg.candidate.version)))
+	def describe(pkg):
+		# Python 3.7 equivalent:
+		# return {"Name": pkg.name, "Installed": pkg.installed.version, "Candidate": pkg.candidate.version}
+		return OrderedDict((("Name", pkg.name), ("Current", pkg.installed.version), ("Target", pkg.candidate.version)))
 
-desc = [describe(pkg) for pkg in upgrades]
-widths = OrderedDict((x, len(x)) for x in desc[0]) # Start with header widths
-for d in desc:
-	for col in d:
-		widths[col] = max(widths[col], len(d[col]))
-fmt = "  ".join("%%-%ds" % col for col in widths.values())
-print(fmt % tuple(widths))
-print("  ".join("-" * col for col in widths.values()))
-for d in desc:
-	print(fmt % tuple(d.values()))
+	desc = [describe(pkg) for pkg in upgrades]
+	widths = OrderedDict((x, len(x)) for x in desc[0]) # Start with header widths
+	for d in desc:
+		for col in d:
+			widths[col] = max(widths[col], len(d[col]))
+	fmt = "  ".join("%%-%ds" % col for col in widths.values())
+	print(fmt % tuple(widths))
+	print("  ".join("-" * col for col in widths.values()))
+	for d in desc:
+		print(fmt % tuple(d.values()))
 
-print()
-if auto: print("Plus %d auto-installed packages." % auto)
+	print()
+	if auto: print("Plus %d auto-installed packages." % auto)
+
+if __name__ == "__main__":
+	main()
