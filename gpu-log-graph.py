8500314c2b423963f2769207d38b042c5bafa8f0
Chris Angelico <rosuav@gmail.com>
2020-02-14 21:35:16 +1100

    gpu-log-graph: Mention how to show GPU rather than VRAM usage

--- gpu-log-graph.py
+++ gpu-log-graph.py
@@ -34,6 +34,8 @@ print(len(data), "data points.")
 def graph_processes():
 	processes = collections.defaultdict(lambda: [0] * len(data))
 	processes["All"] # Put it first. It should perfectly track the Total, which comes from the vram figure.
+	# Graph proc["fb"] aka "framebuffer usage (MB)" to show VRAM usage
+	# Graph proc["sm"] aka "??????? I have no idea what that stands for ??????" to show GPU usage
 	for i, entry in enumerate(data):
 		for proc in entry["processes"]:
 			processes[proc["command"]][i] = int(proc["fb"])
@@ -41,6 +43,7 @@ def graph_processes():
 	for label, usage in processes.items():
 		plt.plot(usage, label=label)
 	graph([d["vram"] * 40.96 for d in data], label="Total") # Rescale from percentage to megabytes (I don't have total VRAM in these stats but it's 4096MB for me)
+	# graph([d["gpu-util"] for d in data], label="Total") # Or graph total GPU usage
 	plt.legend()
 	plt.show()
 
