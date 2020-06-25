36a54416b1075ef9eb1c557b1a74fce7a2d1c185
Chris Angelico <rosuav@gmail.com>
2020-06-25 15:52:07 +1000

    emotify: Handle modified emotes.
    Now that tiered subs to partnered channels can permanently access emote
    modifications, it actually becomes viable to use them in contexts where
    emotify might be called upon. Unfortunately there's no easy way to know
    which modifications are valid, but they all seem to be two-letter tags.

--- emotify.py
+++ emotify.py
@@ -145,6 +145,12 @@ def convert_emotes(msg):
 	emotes = get_emote_list()
 	words = msg.split()
 	for i, word in enumerate(words):
+		if "_" in word:
+			base, tag = word.split("_", 1)
+			if base in emotes and len(tag) == 2:
+				words[i] = '![%s](%s "%s")' % (word, emotes[base].replace("/1.0", "_" + tag + "/1.0"), word)
+				continue
+			# Otherwise fall through. Maybe there are actual emotes with underscores.
 		if word not in emotes: continue
 		words[i] = '![%s](%s "%s")' % (word, emotes[word], word) # Assumes emote names never contain double quotes
 	return " ".join(words)
