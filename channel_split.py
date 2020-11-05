aa1e6dbb5bdf208934056e63b5fb48735bb40069
Chris Angelico <rosuav@gmail.com>
2020-11-05 18:47:03 +1100

    channel_split: Simplify filtergraph creation

--- channel_split.py
+++ channel_split.py
@@ -35,17 +35,14 @@ for line in p.stdout.decode().split("\n"):
 		k, v = line.split(maxsplit=1)
 		cur[k] = v
 
-# Build up a filter_complex that will split the channels out.
-# Also, give each channel a descriptive title eg "[FR] front right".
-filter = "channelsplit=channel_layout=" + audio["channel_layout"]
-map_audio = []
+# Give each channel a descriptive title eg "[FR] front right".
+audio_titles = []
 for i, channel in enumerate(layouts[audio["channel_layout"]].split("+")):
-	filter += f"[{channel}]"
-	map_audio.extend(["-map", f"[{channel}]", "-metadata:s:a:%d" % i, f"title=[{channel}] {channel_desc[channel]}"])
+	audio_titles.extend(["-metadata:s:a:%d" % i, f"title=[{channel}] {channel_desc[channel]}"])
 
 # Let's do this.
-args = (["ffmpeg", "-y", "-i", infile, "-filter_complex", filter]
-	+ map_video + map_audio + map_subs
+args = (["ffmpeg", "-y", "-i", infile, "-filter_complex", "channelsplit=channel_layout=" + audio["channel_layout"]]
+	+ map_video + map_subs + audio_titles
 	+ ["-c:v", "copy", "-c:s", "copy", "-loglevel", "error", "-stats", outfile])
 # print(args)
 sys.exit(subprocess.run(args).returncode)
