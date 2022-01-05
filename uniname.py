86deb4f5c87aa13e4fbb2ada0a3821014de11d64
Chris Angelico <rosuav@gmail.com>
2022-01-06 09:01:40 +1100

    uniname: Jot down a TODO

--- uniname.py
+++ uniname.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 import sys, unicodedata
 for ch in " ".join(sys.argv[1:]): print(ascii(ch), unicodedata.name(ch))
+#TODO: Unicode variant analyzer
+#- Accept parameter which may or may not have a variant selector
+#- Show the base character with every variant selector; highlight the one provided
+#- Make it easy to copy/paste into a browser
+#- There are technically sixteen, but skip 4-14 (FE03-FE0D) as they're not currently used
