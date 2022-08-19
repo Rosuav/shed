d58a33c749e00edabc7db5cedaf90962fd0944e9
Chris Angelico <rosuav@gmail.com>
2022-08-19 20:31:07 +1000

    weakest_link: Plan out the types of links that we will recognize

--- weakest_link.py
+++ weakest_link.py
@@ -15,24 +15,55 @@
 #    a. <nothing here yet - cf "is it an elephant">
 #    b. Log the failure
 # 6. Progressively go through the log and add autofixers
+from urllib.parse import urlparse, urljoin, ParseResult
 from bs4 import BeautifulSoup
 root = "/home/rosuav/gsarchive/live"
 
 scanned = { }
 awaiting = []
-def fix(url):
+
+def report(*msg):
+	# TODO: Log to file?
+	print(*msg)
+
+def fix(oldurl, newurl, context):
+	report("In", context, "replace", oldurl, "with", newurl)
+
+def link(context, url, *, base="https://gsarchive.net/"):
 	# Make the URL absolute
-	...
+	uri = urljoin(base, url)
+	fn = None
+	match urlparse(uri):
+		case ParseResult(scheme="http", netloc="gsarchive.net"):
+			report("Non-encrypted link within site", context, url)
+		case ParseResult(scheme="http"):
+			report("Non-encrypted link outside site", context, url)
+		case ParseResult(scheme="https", netloc="gsarchive.net") as p:
+			if url.startswith("https:"):
+				# The URL was stored absolute, which is inefficient and vulnerable to error
+				fix(url, p.path)
+			fn = p.path
+		case ParseResult(scheme="https"):
+			report("External link", context, url)
+		case ParseResult():
+			report("Non-HTTP link", context, url)
+		case _:
+			report("Unparseable link", context, url)
+	if not fn: return
 	if fn in scanned: return
 	scanned[fn] = 1
 	awaiting.append(fn)
 
 def find_links(fn):
-	with open(fn, "rb") as f:
+	path = root + fn
+	if path.endswith("/"):
+		# Directory names get handled by index files
+		path += "index.html" # Is this the only name available? If multiple, what priority order?
+	with open(path, "rb") as f:
 		soup = BeautifulSoup(f.read(), "html.parser")
 	print("Got soup")
 
-fix("/")
+link("", "/")
 while awaiting:
 	fn = awaiting.pop()
 	print(fn, "...")
