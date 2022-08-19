adc7055633b91ae4a88753e118cd9e82b6635631
Chris Angelico <rosuav@gmail.com>
2022-08-19 19:57:00 +1000

    linkfixer: Plan out a dead-link finder and fixer for the G&S Archive

---
+++ linkfixer.py
@@ -0,0 +1,15 @@
+# Broken link finder/fixer
+# 1. Starting with a given page, scan for all references
+#    <* href="..."> mainly <a> but also <link>
+#    <* src="..."> eg img, script, style
+#    Anything else?
+# 2. If ref is external (http:// or https:// and not gsarchive.net), record
+#    the domain name in case there's a different sort of error.
+# 3. If ref is unnecessarily-external (pointing to gsarchive.net), autofix
+#    to relative.
+# 4. If ref is internal and the file exists, queue the target for scanning,
+#    if not already queued or scanned.
+# 5. If ref is internal and not found:
+#    a. <nothing here yet - cf "is it an elephant">
+#    b. Log the failure
+# 6. Progressively go through the log and add autofixers
