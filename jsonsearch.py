9309cb7f0f9df6e78dec020f61fe24f16b674551
Chris Angelico <rosuav@gmail.com>
2022-09-19 02:03:12 +1000

    jsonsearch: Create a quick way to search a huge JSON tree

---
+++ jsonsearch.py
@@ -0,0 +1,17 @@
+# TODO: Parameterize. Pass a JSON file name, a JSON blob, or no argument to read stdin.
+import json
+import sys
+
+with open("eu4_parse.json") as f: data = json.load(f)
+
+def search(node, term, path):
+	if isinstance(node, dict): items = node.items()
+	elif isinstance(node, list): items = enumerate(node)
+	elif term in str(node):
+		print("->".join(path) + ":", node)
+		return
+	else: return
+	for k, v in items:
+		search(v, term, path + (k,))
+
+search(data, sys.argv[1], ())
