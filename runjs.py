dbad3ef0f58a5c46f6ebea468acca20ede130f68
Chris Angelico <rosuav@gmail.com>
2023-10-07 11:56:11 +1100

    runjs: Implement the inotify part

--- runjs.py
+++ runjs.py
@@ -4,7 +4,7 @@
 # the specified JavaScript file changes, connected browser clients
 # will be signalled to reload it.
 import websockets # ImportError? pip install websockets
-from watchdog.observers import Observer # ImportError? pip install watchdog
+import watchdog.events, watchdog.observers # ImportError? pip install watchdog
 import asyncio
 import collections
 import hashlib
@@ -12,32 +12,46 @@ import http
 import os.path
 import sys
 
-class HashLoader:
-	def __init__(self, fn):
+class HashLoader(watchdog.events.FileSystemEventHandler):
+	def __init__(self, fn, observer):
 		self.filename = fn
 		self.basename = os.path.basename(fn)
 		self.fetch()
+		observer.schedule(self, fn)
 	def fetch(self):
 		with open(self.filename, "rb") as f:
 			self.content = f.read()
 		self.hash = hashlib.md5(self.content).hexdigest() # sha256 is overkill for a simple cachebuster
 
+	# Conceptually, we want to watch a single file for updates.
+	# However, some editors may write to a separate file, then move it over the
+	# original file. This may not work with this system, and would require the
+	# notification of "move to this name".
+	def on_modified(self, event):
+		prevhash = self.hash
+		self.fetch()
+		if self.hash != prevhash: asyncio.run_coroutine_threadsafe(notify_update(self), loop)
+
 sockets = []
 
 async def connection(sock, path):
-	print("CONNECTION", sock, path)
 	sockets.append(sock)
 	try:
 		async for msg in sock:
 			pass # No client-to-server messages currently needed
 	except websockets.ConnectionClosedError:
 		pass
-	print("CONNECTION GONE", sock, path)
 	sockets.remove(sock)
+
+async def notify_update(loader):
+	print("File changed, sending update")
+	for sock in sockets:
+		await sock.send(loader.basename + "?" + loader.hash)
 
 async def process_request(path, headers):
 	"""Handle the basic HTTP requests needed to bootstrap everything"""
 	if path == "/":
+		print("Serving page.")
 		return (http.HTTPStatus.OK, {"Content-Type": "text/html"}, f"""<!doctype html>
 			<html><head><title>RunJS</title>
 				<script src="/runjs.js" type=module></script>
@@ -50,12 +64,19 @@ async def process_request(path, headers):
 			function connect() {
 				let socket = new WebSocket("ws://" + window.location.host + "/ws");
 				socket.onclose = () => setTimeout(connect, 2500);
-				socket.onmessage = (ev) => console.log("GOT UPDATE", ev.data);
+				socket.onmessage = (ev) => {
+					document.getElementById("runme").remove();
+					const script = document.createElement("script");
+					script.type = "module";
+					script.src = ev.data;
+					document.head.append(script);
+				};
 			}
 			connect();
 		""")
 
 async def main():
+	global loop; loop = asyncio.get_running_loop()
 	async with websockets.serve(connection, "localhost", 8000, process_request=process_request):
 		print("Ready and listening. Press Ctrl-C (maybe twice) to halt.")
 		await asyncio.Future()
@@ -64,6 +85,11 @@ if len(sys.argv) < 2:
 	print("USAGE: python3 %s filename.js" % sys.argv[0])
 	sys.exit(1)
 
-runjs = HashLoader(sys.argv[1])
+observer = watchdog.observers.Observer()
+runjs = HashLoader(sys.argv[1], observer)
+observer.start()
 
-asyncio.run(main())
+try: asyncio.run(main())
+finally:
+	observer.stop()
+	observer.join()
