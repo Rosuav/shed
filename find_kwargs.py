b62b1485e48e641fd3ee0a989ee156fbf8d622a7
Chris Angelico <rosuav@gmail.com>
2023-10-23 09:55:37 +1100

    find_kwargs: Show some stats by percentages

--- find_kwargs.py
+++ find_kwargs.py
@@ -2,10 +2,14 @@
 import ast
 import os
 import sys
+from collections import defaultdict
 
 fn = "(unknown)" # global because I'm lazy
+stats = defaultdict(int)
+
 class ParamFinder(ast.NodeVisitor):
 	def visit_Call(self, node):
+		matches = False
 		for arg in node.keywords:
 			if (isinstance(arg.value, ast.Name) and
 				# arg.value.ctx is Load # can't imagine how it would be otherwise
@@ -14,6 +18,12 @@ class ParamFinder(ast.NodeVisitor):
 						fn, node.lineno, node.col_offset,
 						ast.unparse(node.func), arg.arg, arg.arg
 					))
+					matches = True
+		# For statistical purposes, it may also be of interest to count the
+		# number of arguments, not just the number of calls.
+		stats["Calls"] += 1
+		if node.keywords: stats["Kwargs"] += 1
+		if matches: stats["Matches"] += 1
 
 for root, dirs, files in os.walk("."):
 	for fn in files:
@@ -27,3 +37,11 @@ for root, dirs, files in os.walk("."):
 				print("Unable to parse", fn, file=sys.stderr)
 				print(e, file=sys.stderr)
 			ParamFinder().visit(node)
+
+print()
+if stats["Calls"]:
+	print("Total function calls:", stats["Calls"])
+	print("Calls with any kwarg:", stats["Kwargs"], "%.2f%%" % (stats["Kwargs"] / stats["Calls"] * 100.0))
+	print("Calls with any 'x=x':", stats["Matches"], "%.2f%%" % (stats["Matches"] / stats["Calls"] * 100.0))
+	if stats["Kwargs"]: print(" - compared to kwarg:", stats["Matches"], "%.2f%%" % (stats["Matches"] / stats["Kwargs"] * 100.0))
+
