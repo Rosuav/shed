44e4ae5a26b526f16a1e3d9a607abc60b9d2e0dc
Chris Angelico <rosuav@gmail.com>
2024-07-31 00:42:56 +1000

    memusage: Add a log option

--- memusage.py
+++ memusage.py
@@ -1,11 +1,14 @@
+import sys
 import time
 last = float("inf")
+log = open("memusage.log") if "--log" in sys.argv else None
 while True:
 	with open("/proc/meminfo") as mem:
 		for line in mem:
 			key, val = line.split(":")
 			if key == "MemAvailable":
 				memavail = int(val.replace("kB", ""))
+				if log: print(int(time.time()), memavail, file=log)
 				if memavail < last:
 					print(line.strip())
 					last = memavail
