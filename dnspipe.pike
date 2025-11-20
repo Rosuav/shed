481c26d482274ae55b8150a4399f0bba3e427816
Chris Angelico <rosuav@gmail.com>
2025-11-21 10:14:19 +1100

    Start attempting to build the reverse DNS

---
+++ dnspipe.pike
@@ -0,0 +1,43 @@
+//Generate responses to DNS packets sent on stdin
+//Python's tools for DNS management aren't all that good, so we drop to Pike for this part.
+
+//For decode_res and low_send_reply (the latter of which is protected, so we inherit rather than instantiating)
+inherit Protocols.DNS.server_base;
+
+mapping dns_response(mapping req) {
+	mapping q = req->qd[0];
+	if (q->type == Protocols.DNS.T_PTR) {
+		sscanf(q->name, "%x.%x.%x.%x.%s", int d, int c, int b, int a, string tail);
+		//This should be the only domain that's delegated to us
+		if (tail != "0.0.0.0.0.0.0.0.0.0.0.0.1.0.0.0.8.4.f.b.3.0.8.5.3.0.4.2.ip6.arpa") return (["rcode": Protocols.DNS.NXDOMAIN]);
+		if (a) return (["rcode": Protocols.DNS.NXDOMAIN]);
+		string ptr;
+		if (b) {
+			//eg ::01xx, this is a step in the chain. The last two digits are the step number, and b is the document number.
+			int step = c << 4 + d;
+			ptr = "step-" + step + ".jabberwocky.";
+		} else if (d) {
+			//eg ::000x, this is the endpoint of a chain
+			if (d == 1) ptr = "jabberwocky.rosuav.com.";
+		} else {
+			//It's ::0000, the DNS server itself.
+			ptr = "tomfoolery.rosuav.com.";
+		}
+		if (!ptr) return (["rcode": Protocols.DNS.NXDOMAIN]);
+		return (["an": (["cl": q->cl, "ttl": 600, "type": q->type, "name": q->name, "ptr": ptr])]);
+	}
+}
+
+int main() {
+	while (1) catch {
+		string line = Stdio.stdin->gets();
+		if (!line || !sizeof(line)) break;
+		werror("Got a packet\n");
+		sscanf(line, "%s %d %s", string ip, int port, string pkt);
+		mapping req = decode_res(MIME.decode_base64(pkt));
+		pkt = low_send_reply(dns_response(req), req, ([]));
+		werror("SENDING RESP %O %O\n", ip, port);
+		write("%s %d %s\n", ip, port, MIME.encode_base64(pkt, 1));
+		Stdio.stdout->flush();
+	};
+}
--- longtrace.py
+++ longtrace.py
@@ -1,6 +1,15 @@
-from random import randrange
+import base64
 import socket
-from netfilterqueue import NetfilterQueue
+import subprocess
+import threading
+from netfilterqueue import NetfilterQueue # pip install netfilterqueue
+
+# Everything here is in the address space 2403:5803:bf48:1::xxxx.
+# This is magically routed to the netfilter queue and handled by this script.
+# Currently, all relevant addresses are in 2403:5803:bf48:1::/112 but this may change in the future.
+# ::0000 is the DNS server
+# ::0001 is the target for the first trace (Jabberwocky)
+# ::01xx is the steps of the first trace, where xx is the hop count.
 
 def icmpv6_checksum(src, dest, pkt):
 	"""Calculate an ICMPv6 checksum for a packet
@@ -18,40 +27,69 @@ def icmpv6_checksum(src, dest, pkt):
 		checksum = (checksum >> 16) + (checksum & 0xFFFF)
 	return ~checksum & 0xFFFF
 
+dnshandler = subprocess.Popen(["pike", "dnspipe.pike"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=0)
+def dnsresp():
+	buf = b""
+	while True:
+		while b"\n" not in buf:
+			chunk = dnshandler.stdout.read(1024)
+			print("GOT CHUNK", chunk)
+			if not chunk: return
+			buf += chunk
+		msg, _, buf = buf.partition(b"\n")
+		ip, port, data = msg.split(b" ")
+		data = base64.b64decode(data)
+		print("Send to", ip, port)
+threading.Thread(target=dnsresp).start()
+
 def handle_packet(pkt):
 	data = pkt.get_payload()
 	src = socket.inet_ntop(socket.AF_INET6, data[8:8+16])
 	dest = socket.inet_ntop(socket.AF_INET6, data[24:24+16])
 	print(pkt, "from", src, "to", dest, "TTL", data[7])
 	pkt.drop()
+	# Is it a UDP packet on port 53? That's what we in the biz call "DNS"!
+	if data[6] == 17 and dest == "2403:5803:bf48:1::" and data[42:44] == b"\0\x35":
+		print("That looks like DNS to me!")
+		port = data[40] * 256 + data[41]
+		dnshandler.stdin.write(b"%s %d %s\n" % (src.encode(), port, base64.b64encode(data[48:])));
+		return
 	# If we've reached the end of the trace (here, arbitrarily set at 10 hops), send back "Port unreachable",
 	# otherwise send back "Time exceeded".
 	if data[7] >= 10:
-		srcaddr = data[24:24+16] # Response comes back from the actual destination
+		srcaddr = dest # Response comes back from the actual destination
 		resp = b"\1\4\0\0\0\0\0\0" + data[:48]
 	else:
 		# Response comes back from a mythical hop between here and there
-		srcaddr = socket.inet_pton(socket.AF_INET6, "2403:5803:bf48:1::1")[:-2] + b"\1" + data[7:8]
+		srcaddr = "2403:5803:bf48:1::%x" % (0x100 + data[7])
 		resp = b"\3\0\0\0\0\0\0\0" + data[:48]
-	checksum = icmpv6_checksum(srcaddr, data[8:8+16], resp)
+	checksum = icmpv6_checksum(socket.inet_pton(socket.AF_INET6, srcaddr), data[8:8+16], resp)
 	resp = resp[:2] + checksum.to_bytes(2, "big") + resp[4:]
+	send_ipv6(srcaddr, src, resp)
+
+def send_ipv6(src, dest, pkt):
 	# Prepend an IPv6 header to the ICMP packet.
 	# TODO: Randomize the 20-bit flow label (here 0x12345) once I no longer need to be able to spot it in wireshark
-	resp = b"\x60\x01\x23\x45" + len(resp).to_bytes(2, "big") + b"\x3a\x40" + srcaddr + data[8:8+16] + resp
+	srcaddr = socket.inet_pton(socket.AF_INET6, src)
+	destaddr = socket.inet_pton(socket.AF_INET6, dest)
+	resp = b"\x60\x01\x23\x45" + len(pkt).to_bytes(2, "big") + b"\x3a\x40" + srcaddr + destaddr + pkt
 	sock = socket.socket(socket.AF_INET6, socket.SOCK_RAW, socket.IPPROTO_ICMPV6)
 	# So, we need to send this from a specific origin address. Option 1: Have every one of those
 	# addresses as an actual bindable address on the network interface. Bit of a pain. I don't
-	# know how 127.x.y.z works, as you can bind to any addrses within that range, but I haven't
+	# know how 127.x.y.z works, as you can bind to any address within that range, but I haven't
 	# been able to replicate that for an IPv6 netblock. So we use option 2: raw socket, no IP
 	# header, and build our own. However, we need to stop the system from adding its own. On an
 	# IPv4 socket, that's pretty straightforward; for some reason, the corresponding sockopt
 	# for IPv6 doesn't seem to be listed in the Python socket module, at least not in 3.14.
 	# Fortunately, the underlying kernel does support it, and it's socket option 36.
 	sock.setsockopt(socket.IPPROTO_IPV6, 36, 1)
-	sock.sendto(resp, (src, 0))
+	sock.sendto(resp, (dest, 0))
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(1, handle_packet)
 try: nfqueue.run()
 except KeyboardInterrupt: print()
-finally: nfqueue.unbind()
+finally:
+	nfqueue.unbind()
+	dnshandler.stdin.close()
+	dnshandler.wait()
