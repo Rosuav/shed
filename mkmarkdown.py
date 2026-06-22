ef18e4c65b8cc100115426866c01c787f30308b9
Chris Angelico <rosuav@gmail.com>
2026-06-22 12:20:30 +1000

    mkmarkdown: Implement a basic templating engine

--- mkmarkdown.py
+++ mkmarkdown.py
@@ -1,12 +1,49 @@
 import markdown
 import xml.etree.ElementTree as ET
 
+# TODO: Iterate over some directory, loading the templates from it
+# For now, hard-coding this one simple template.
+templates = {
+	"default.html": """<!DOCTYPE HTML>
+<html lang=en>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>$$title$$</title>
+<link rel="stylesheet" href="styles.css">
+<link rel="icon" href="siteicon.png">
+$$head_scripts$$</head>
+<body><main>$$content$$</main></body>
+</html>
+""",
+}
+
+# This would come from the database, or be directly provided by the user during
+# editing. For now, simple example.
+page = """\
+script: foo.js foo.css
+
+# Hello, world
+
+* [Simple link](dest)
+* [This is a link](to/here :.cls #someid)
+* [Button](:.btn)
+
+Sidebar
+{:tag=aside}
+
+"""
+
 # Taking advantage of the existing attr_list extension here
 from markdown.extensions.attr_list import AttrListTreeprocessor
 class LinkAttrsTP(AttrListTreeprocessor):
 	def run(self, element):
 		super().run(element)
+		self.md.first_heading = None
 		for child in list(element.iter()):
+			if child.tag == "h1":
+				if self.md.first_heading is None:
+					self.md.first_heading = child.text
 			if child.tag == "a":
 				# Two syntaxes: Links with attributes and buttons
 				# [Link text](dest :.cls #id)
@@ -40,13 +77,26 @@ class LinkAttrs(markdown.extensions.Extension):
 		# best, as this allows us to take advantage of all the previous work.
 		md.treeprocessors.register(LinkAttrsTP(md), "link_attrs", 0)
 
-print(markdown.markdown("""# Hello, world!
+md = markdown.Markdown(extensions=["meta", LinkAttrs()])
+html = md.convert(page)
+sitetitle = md.Meta.get("sitetitle", "Example Site")
+title = md.Meta.get("title") or (md.first_heading,)
+title = " ".join(title) + " - " + sitetitle if title else sitetitle
+def make_script(fn):
+	if fn.endswith(".js"):
+		return "<script type=module src=\"" + fn + "\"></script>"
+	if fn.endswith(".css"):
+		return "<link rel=\"stylesheet\" href=\"" + fn + "\">"
+	return "<!-- unknown script " + fn + "-->"
 
-* [Simple link](dest)
-* [This is a link](to/here :.cls #someid)
-* [Button](:.btn)
-
-Sidebar
-{:tag=aside}
-
-""", extensions=[LinkAttrs()]))
+head_scripts = "".join(make_script(fn) for line in md.Meta.get("script", []) for fn in line.split())
+template = "".join(md.Meta.get("template", ["default.html"]))
+if template not in templates:
+	# TODO: Throw some kind of error, or at least a warning
+	template = "default.html"
+output = (templates[template]
+	.replace("$$title$$", title)
+	.replace("$$head_scripts$$", head_scripts)
+	.replace("$$content$$", html)
+)
+print(output)
