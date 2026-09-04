7488bc63f24ed4436ec5c920ddd57a06d2aa772d
Chris Angelico <rosuav@gmail.com>
2026-09-04 22:10:53 +1000

    monobuild: If an assembly isn't found, bail immediately

--- monobuild.py
+++ monobuild.py
@@ -27,6 +27,7 @@ with open(fn) as f:
 					break
 			else:
 				print("Unable to find assembly: ", basename, file=sys.stderr)
+				sys.exit(1)
 cmd.append(fn)
 try: subprocess.run(cmd, check=True)
 except subprocess.CalledProcessError: sys.exit(1) # It's okay to suppress the traceback, but we still want to exit 1
